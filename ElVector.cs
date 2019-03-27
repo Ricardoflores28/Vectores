1490511c0e560526cbd5a326454f7535af3027f5
Ricardoflores28 <48325337+Ricardoflores28@users.noreply.github.com>
2019-03-27 00:12:34 -0600

    Add files via upload

---
+++ ElVector.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectores
+{
+    class ElVector
+    {
+
+
+        private int[] vector;
+
+        public ElVector(int tamaño)
+        {
+            vector = new int[tamaño];
+
+        }
+        public void GirarArriba()
+        {
+            int temporal;
+            temporal = vector[vector.Length - 1];
+
+            for (int i = vector.Length - 1; i >= 1; i--)
+                vector[i] = vector[i - 1];
+            vector[0] = temporal;
+
+
+        }
+        public void GirarAbajo()
+        {
+            int temporal;
+            temporal = vector[0];
+
+            for (int i = 0; i<vector.Length - 1; i++)
+                vector[i] = vector[i + 1];
+            vector[vector.Length - 1] = temporal;
+        }
+        public void Invertir()
+        {
+            int temporal, medio;
+            medio = vector.Length / 2;
+            for (int i = 0; i < medio; i++)
+            {
+                temporal = vector[i];
+                vector[i] = vector[vector.Length - 1 - i];
+                vector[vector.Length - 1 - i] = temporal;
+            }
+
+        }
+
+
+    
+        
+                public void llenar(int numero)
+
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = numero;
+                numero++;
+            }
+
+
+        }
+
+
+
+        public string Mostrar()
+        {
+            string guardar = " ";
+            for (int i = 0; i < vector.Length; i++)
+            {
+                guardar += "[" + i + "]=" + vector[i] + Environment.NewLine;
+            }
+            return guardar;
+
+
+        }
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vectores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ElVector vec;
+        private void b1_Click(object sender, EventArgs e)
+        {
+            vec = new ElVector(Convert.ToInt32(t1.Text));
+           
+        }
+        
+        private void b2_Click(object sender, EventArgs e)
+        {
+            vec.llenar(Convert.ToInt32(t2.Text));
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            t3resultados.Text = vec.Mostrar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vec.GirarArriba();
+            t3resultados.Text = vec.Mostrar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vec.GirarAbajo();
+            t3resultados.Text = vec.Mostrar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vec.Invertir();
+            t3resultados.Text = vec.Mostrar();
+        }
+    }
+}
